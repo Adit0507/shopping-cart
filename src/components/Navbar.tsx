b76b0179e878de129a0bc15ef4fbc0242bcf0b03
Adit0507 <adi05072002@gmail.com>
2022-08-11 19:37:32 +0530

    Adding sticky feature in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
