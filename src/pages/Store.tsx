a702f00e36c28a6509af792c7aa9b4ee296a7a7c
Adit0507 <adi05072002@gmail.com>
2022-08-11 19:47:34 +0530

    Working on the Store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import storeItems from "../data/items.json"
+import {Row, Col} from "react-bootstrap";
+
 export function Store() {
-    return <h1>Store</h1>
+    return <><h1>Store</h1>
+    {/* md => For med. screen size we want 2 cols
+    xs => For extra small size we want 1 col
+    lg => for large size we want 3 cols
+    */}
+    <Row md={2} xs={1} lg={3} className="g-3" >
+        {storeItems.map(item => (
+            <Col>{JSON.stringify(item)}</Col>
+
+        ))}
+    </Row>
+    
+    </>
 }
