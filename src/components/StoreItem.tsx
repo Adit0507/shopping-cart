f3c3c72f28767e626bd48d7165c9577baec62046
Adit0507 <adi05072002@gmail.com>
2022-09-21 17:25:36 +0530

    Working on StoreItem component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -16,6 +16,13 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         height="200px"
         style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+          {/* fs -> font size */}
+          <span className="fs-2" >{name}</span>
+          <span className="ms-2 text-muted" >{price}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 }
